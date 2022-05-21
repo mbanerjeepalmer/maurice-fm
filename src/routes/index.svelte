2b06c28cb761d0e19c8f9c728ac189274c48da57
mbanerjeepalmer <mbanerjeepalmer@gmail.com>
2022-05-21 22:47:26 +0100

    Remove other HTML cruft

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,22 +2,12 @@
 	import Title from '$lib/Title.svelte';
 </script>
 
-<html lang="en">
-	<head>
-		<meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<meta http-equiv="X-UA-Compatible" content="ie=edge" />
-		<title>maurice</title>
-	</head>
-	<body>
-		<Title>What I've learnt so far</Title>
-		<p>A collection of practical how-to guides based on experience.</p>
-		<ul>
-			<li><a class="underline" href="./make-better-slides">Make better slides</a></li>
-			<li><a class="underline" href="./learn-to-code">Learn to code in 15 minutes</a></li>
-			<li><a class="underline" href="./useful-user-research">Useful user research</a></li>
-		</ul>
-		<br />
-		Ciao, Maurice
-	</body>
-</html>
+<Title>What I've learnt so far</Title>
+<p>A collection of practical how-to guides based on experience.</p>
+<ul>
+	<li><a class="underline" href="./make-better-slides">Make better slides</a></li>
+	<li><a class="underline" href="./learn-to-code">Learn to code in 15 minutes</a></li>
+	<li><a class="underline" href="./useful-user-research">Useful user research</a></li>
+</ul>
+<br />
+Ciao, Maurice
