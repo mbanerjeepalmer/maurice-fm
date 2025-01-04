eb4cbf9c134aa19b97b20a47642ee3e0235b725c
Maurice <mbanerjeepalmer@gmail.com>
2025-01-04 14:28:56 +0000

    Updated homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,31 +3,87 @@
 </script>
 
 <Title>Maurice Banerjee Palmer</Title>
-Working on <a class="underline" href="https://bnqt.app">Banquet: the best use of your attention.</a>
-<p>Read the latest <a class="underline" href="./banquet-wave">Banquet update</a>.</p>
-<p>
-	Find me on:
-	<a href="https://linkedin.com/in/mbanerjeepalmer">LinkedIn</a>,
-	<a href="https://github.com/mbanerjeepalmer">GitHub</a>.
-</p>
-
-<Title>What I've learnt so far</Title>
+<h2>What I've learnt so far</h2>
 <p>
 	A collection of how-to guides based on what I've learnt so far as a product person. It's a work in
 	progress.
 </p>
 <ul>
-	<li><a class="underline" href="./hacketisms">Hacketisms: Catechisms for a hackathon</a></li>
-	<li><a class="underline" href="./rube">A Rube Golderbergian Boilerplate for Claude</a></li>
+	<li><a href="./hacketisms">Hacketisms: Catechisms for a hackathon</a></li>
+	<li><a href="./rube">A Rube Golderbergian Boilerplate for Claude</a></li>
 	<li>
-		<a class="underline" href="./passable-cv">Write a passable CV</a>
+		<a href="./passable-cv">Write a passable CV</a>
 	</li>
 	<li>
-		<a class="underline" href="./money-transfer-companies">International money transfer companies</a
-		>
+		<a href="./money-transfer-companies">International money transfer companies</a>
 	</li>
-	<li><a class="underline" href="./make-better-slides">Make better slides</a></li>
-	<li><a class="underline" href="./presenting-your-slides">Presenting your slides</a></li>
-	<li><a class="underline" href="./learn-to-code">Learn to code in 15 minutes</a></li>
-	<li><a class="underline" href="./useful-user-research">Useful user research</a></li>
+	<li><a href="./make-better-slides">Make better slides</a></li>
+	<li><a href="./presenting-your-slides">Presenting your slides</a></li>
+	<li><a href="./learn-to-code">Learn to code in 15 minutes</a></li>
+	<li><a href="./useful-user-research">Useful user research</a></li>
 </ul>
+<h2>What I've done</h2>
+<h3><a href="https://bnqt.app" target="_blank" rel="noopener noreferrer">Banquet</a></h3>
+<p>
+	A media curation web app. The best use of your attention. <a href="./banquet-wave">Read</a> an
+	update,
+	<a href="https://imgur.com/wrh19Uf" target="_blank" rel="noopener noreferrer">look</a> at the UI.
+</p>
+<h3><a href="https://printstack.net" target="_blank" rel="noopener noreferrer">Printstack</a></h3>
+<p>
+	Turn a Substack into a book. Also <a
+		href="https://printstack.substack.com"
+		target="_blank"
+		rel="noopener noreferrer">exists as a Substack</a
+	>.
+</p>
+<h3><a href="https://vol.best" target="_blank" rel="noopener noreferrer">vol</a></h3>
+<p>
+	Search for podcasts using your ambitions instead of keywords. Built solo at Meta's Llama
+	hackathon. See <a href="https://github.com/mbanerjeepalmer/vol-web">repo</a>
+	and
+	<a href="https://www.producthunt.com/posts/vol" target="_blank" rel="noopener noreferrer"
+		>Product Hunt launch</a
+	>.
+</p>
+<h3>
+	<a
+		href="https://www.linkedin.com/in/mbanerjeepalmer/details/experience/"
+		target="_blank"
+		rel="noopener noreferrer"
+	>
+		Seven years in product management</a
+	>
+</h3>
+<p>
+	Mostly in FinTech. At some companies you might have heard of, like Amex; backed by some investors
+	you might have heard of, like Andreesen Horowitz.
+</p>
+<h3 class="font-medium">And some consulting</h3>
+<p>User research training, product management training, technical writing.</p>
+<h2>Find me</h2>
+<p>If anything above interests you,I'm always happy to chat.</p>
+<p>
+	<a href="https://github.com/mbanerjeepalmer" target="_blank" rel="noopener noreferrer">GitHub</a>,
+	<a href="https://linkedin.com/in/mbanerjeepalmer" target="_blank" rel="noopener noreferrer"
+		>LinkedIn</a
+	>, <a href="https://x.com/mbanerjeepalmer" target="_blank" rel="noopener noreferrer">X</a>,
+	<a
+		href="https://bsky.app/profile/mbanerjeepalmer.bsky.social"
+		target="_blank"
+		rel="noopener noreferrer">Bluesky</a
+	>.
+</p>
+
+<style lang="postcss">
+	a {
+		@apply underline;
+	}
+	/* h2 {
+		@apply mt-16;
+	}
+
+	h3 {
+		@apply mt-12;
+	} */
+</style>
