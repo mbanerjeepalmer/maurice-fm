7415e6bcd4e798d09a06c4dd50a4b247acc2c232
mbanerjeepalmer <31225563+mbanerjeepalmer@users.noreply.github.com>
2026-05-13 10:15:40 -0600

    formatting + reorganise sections

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,11 +2,11 @@
 	import Title from '$lib/Title.svelte';
 </script>
 
-<Title>Maurice Banerjee Palmer</Title>
+<title>Maurice Banerjee Palmer</title>
 <h2>What I've learnt so far</h2>
 <p>
-	A collection of how-to guides based on what I've learnt so far as a product person. It's a work
-	in progress.
+	A collection of how-to guides based on what I've learnt so far as a product person. It's a work in
+	progress.
 </p>
 <ul>
 	<li><a href="./hacketisms">Hacketisms: Catechisms for a hackathon</a></li>
@@ -26,9 +26,8 @@
 <ul>
 	<li><a href="./finest-tv">The final week of the finest television</a></li>
 	<li>
-		<a
-			href="https://digitalfrontier.com/articles/rss-dead-protocols-internet-llm"
-			target="_blank">RSS is dead. Long live RSS!</a
+		<a href="https://digitalfrontier.com/articles/rss-dead-protocols-internet-llm" target="_blank"
+			>RSS is dead. Long live RSS!</a
 		>
 	</li>
 	<li>
@@ -43,14 +42,30 @@
 	</li>
 </ul>
 <h2>What I've done</h2>
-<h3><a href="https://bnqt.app" target="_blank" rel="noopener noreferrer">Banquet</a></h3>
+<h3>Consulting (now)</h3>
+<ul>
+	<li>
+		AI for a 230-year-old auction house. Trained agentic engineering, shipped new products at 20x
+		normal pace.
+	</li>
+	<li>User research training for a VC-backed proptech startup.</li>
+</ul>
+<h3><a href="https://bnqt.app/" target="_blank" rel="noopener noreferrer">Banquet</a></h3>
 <p>
-	The best use of your attention. <a href="./banquet-wave">Read</a> an update,
-	<a href="https://imgur.com/wrh19Uf" target="_blank" rel="noopener noreferrer">look</a> at the UI.
+	<a href="https://bnqt.app/" target="_blank" rel="noopener noreferrer">Banquet</a>: Solo founded AI
+	media startup building products to make the best use of your attention.
 </p>
-<h3><a href="https://printstack.net" target="_blank" rel="noopener noreferrer">Printstack</a></h3>
+<p>
+	I decided to stop after two years. I'll write a post-mortem at some point. For now you can read an
+	early update <a href="./banquet-wave">here</a>). If you're working on something similar and want
+	to learn about my experiences please drop me a line.
+</p>
+
+<h4><a href="https://anatole.fyi" target="_blank" rel="noopener noreferrer">Anatole</a></h4>
+<p>A Slack bot that keeps you on top of everything you need to know for work.</p>
+<h4><a href="https://printstack.net" target="_blank" rel="noopener noreferrer">Printstack</a></h4>
 <p>Turn a Substack into a book.</p>
-<h3><a href="https://vol.best" target="_blank" rel="noopener noreferrer">vol</a></h3>
+<h4><a href="https://vol.best" target="_blank" rel="noopener noreferrer">vol</a></h4>
 <p>
 	Search for podcasts using your ambitions instead of keywords. Built solo at Meta's Llama
 	hackathon. See <a href="https://github.com/mbanerjeepalmer/vol-web">repo</a>
@@ -58,8 +73,33 @@
 	<a
 		href="https://www.producthunt.com/products/vol/launches/vol-2"
 		target="_blank"
-		rel="noopener noreferrer">Product Hunt #9 of the day</a
+		rel="noopener noreferrer"
+		>Product Hunt #9 of the day</a
 	>.
+</p>
+<h3>Filmmaking</h3>
+<p>
+	Helped produce and shoot
+	<a
+		href="https://filmsandfestivals.britishcouncil.org/projects/elephant-families"
+		target="_blank"
+		rel="noopener noreferrer"
+		>Elephant Families</a
+	>, a short documentary about the Latino community in South London. The film was selected for the
+	<a
+		href="https://whatson.bfi.org.uk/lff/Online/default.asp?BOparam::WScontent::loadArticle::permalink=discovering-home-lff25&BOparam::WScontent::loadArticle::context_id="
+		target="_blank"
+		rel="noopener noreferrer"
+		>BFI London Film Festival</a
+	>
+	and
+	<a
+		href="https://boxoffice.hotdocs.ca/websales/pages/info.aspx?evtinfo=608269~cd63fa6a-1569-492d-89ae-a73ea714567e&epguid=f3bf8433-2ddd-4eb0-a2b5-e241bcf1021b&mdy=5/3/2026&"
+		target="_blank"
+		rel="noopener noreferrer"
+		>Hot Docs</a
+	>
+	in Toronto.
 </p>
 <h3>
 	<a
@@ -71,23 +111,21 @@
 	>
 </h3>
 <p>
-	Mostly in FinTech. At some companies you might have heard of, like Amex. Backed by some
-	investors you might have heard of, like Andreesen Horowitz.
+	Mostly in FinTech. At some companies you might have heard of, like Amex. Backed by some investors
+	you might have heard of, like Andreesen Horowitz.
 </p>
-<h3 class="font-medium">And some consulting</h3>
-<p>User research training, AI engineering, product management training, technical writing.</p>
 <h2>Find me</h2>
 <p>If anything above interests you,I'm always happy to chat.</p>
 <p>
-	<a href="https://github.com/mbanerjeepalmer" target="_blank" rel="noopener noreferrer">GitHub</a
-	>,
+	<a href="https://github.com/mbanerjeepalmer" target="_blank" rel="noopener noreferrer">GitHub</a>,
 	<a href="https://linkedin.com/in/mbanerjeepalmer" target="_blank" rel="noopener noreferrer"
 		>LinkedIn</a
 	>, <a href="https://x.com/mbanerjeepalmer" target="_blank" rel="noopener noreferrer">X</a>,
 	<a
 		href="https://bsky.app/profile/mbanerjeepalmer.bsky.social"
 		target="_blank"
-		rel="noopener noreferrer">Bluesky</a
+		rel="noopener noreferrer"
+		>Bluesky</a
 	>, <a href="mailto:hello@maurice.fm" target="_blank" rel="noopener noreferrer">email</a>.
 </p>
 
